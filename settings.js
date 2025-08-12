7f5695a574ea10f05bb99f9589d21a11ee19ddef
Mouhathiam <165289125+Mouhathiam@users.noreply.github.com>
2025-08-12 02:06:07 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '2347063956321'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '‪221768818935‬'  //creator number
+global.ownername = 'Precious mouha😈👑' //owner name
 global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
 
 //sticker details
